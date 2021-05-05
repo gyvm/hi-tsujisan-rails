cbece21358a97b1f68088fca7bca2501c575c733
gyvm <jolcde75+github@gmail.com>
2021-05-06 08:44:29 +0900

    イベント作成APIの追加

--- app/controllers/api/v1/events_controller.rb
+++ app/controllers/api/v1/events_controller.rb
@@ -1,2 +1,23 @@
 class Api::V1::EventsController < ApplicationController
+  require 'date'
+  require 'securerandom'
+
+  def create
+    str_unixtime = Time.now.to_i.to_s
+    shared_url = SecureRandom.alphanumeric(10) + str_unixtime
+
+    ActiveRecord::Base.transaction do
+      @event = Event.create(event_params)
+      @event.create_url(url: shared_url)
+    end
+    render json: { status: 'SUCCESS', event: @event }
+  rescue StandardError => e
+    render json: { status: 'FAILED', error: e.message }
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:name, :description, :possible_dates, :event_date)
+  end
 end
