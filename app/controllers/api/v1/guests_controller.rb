26760cac9ecb49e0acbcee60c0de4844cb898182
gyvm <jolcde75+github@gmail.com>
2021-05-06 08:45:18 +0900

    参加者の参加可能日登録APIの追加

--- app/controllers/api/v1/guests_controller.rb
+++ app/controllers/api/v1/guests_controller.rb
@@ -1,2 +1,17 @@
 class Api::V1::GuestsController < ApplicationController
+  def create
+    ActiveRecord::Base.transaction do
+      @event = Event.find(Url.find_by(url: params[:url]).event_id)
+      @event.guests.create
+
+      render json: { status: 'SUCCESS', guest: @event.guests }
+    end
+  end
+
+  private
+
+  def guest_params
+    # params.require(:guest).permit(:name, :comment, events_id: @event_id, possible_dates: {})
+    params.require(:guest).permit(:name, :comment, possible_dates: {})
+  end
 end
