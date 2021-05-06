e6a44a03cfff50f862fdd5bce543974e2ff5234b
gyvm <jolcde75+github@gmail.com>
2021-05-06 09:50:47 +0900

    ゲスト予定登録機能の修正

--- app/controllers/api/v1/guests_controller.rb
+++ app/controllers/api/v1/guests_controller.rb
@@ -2,16 +2,16 @@ class Api::V1::GuestsController < ApplicationController
   def create
     ActiveRecord::Base.transaction do
       @event = Event.find(Url.find_by(url: params[:url]).event_id)
-      @event.guests.create
-
-      render json: { status: 'SUCCESS', guest: @event.guests }
+      @event.guests.create(guest_params)
     end
+    render json: { status: 'SUCCESS', guest: @event.guests }
+  rescue StandardError => e
+    render json: { status: 'FAILED', error: e.message }
   end
 
   private
 
   def guest_params
-    # params.require(:guest).permit(:name, :comment, events_id: @event_id, possible_dates: {})
-    params.require(:guest).permit(:name, :comment, possible_dates: {})
+    params.require(:guest).permit(:nickname, :comment, possible_dates: {})
   end
 end
