60a33226ea92c0c996f195dc84e380a0d904c011
gyvm <jolcde75+github@gmail.com>
2021-09-02 02:37:48 +0900

    fix a typo

--- app/models/event.rb
+++ app/models/event.rb
@@ -8,7 +8,7 @@ class Event < ApplicationRecord
     ActiveRecord::Base.connection.raw_connection.prepare('SELECT possible_dates.id, COUNT(*) FROM possible_dates INNER JOIN guest_possible_dates ON possible_dates.id =guest_possible_dates.possible_date_id WHERE event_id=? AND status=1 GROUP BY possible_dates.id;').execute(event_id).to_h
   end
 
-  def get_guest_possible_dates(_event_id)
+  def get_guest_possible_dates(event_id)
     # guest_possible_dates = ActiveRecord::Base.connection.execute('SELECT guests.id, possible_date_id, status FROM guests INNER JOIN guest_possible_dates ON guests.id =guest_possible_dates.guest_id WHERE event_id=62;').to_a
     guest_possible_dates = ActiveRecord::Base.connection.raw_connection.prepare('SELECT guests.id, possible_date_id, status FROM guests INNER JOIN guest_possible_dates ON guests.id =guest_possible_dates.guest_id WHERE event_id=?;').execute(event_id).to_a
     guest_possible_dates.map do |data|
