5aafff8d05c511685929a12e7ad4ea579415760b
gyvm <jolcde75+github@gmail.com>
2021-09-05 04:35:44 +0900

    update puma.rb

--- config/puma.rb
+++ config/puma.rb
@@ -11,15 +11,24 @@ threads min_threads_count, max_threads_count
 # Specifies the `worker_timeout` threshold that Puma will use to wait before
 # terminating a worker in development environments.
 #
-worker_timeout 3600 if ENV.fetch("RAILS_ENV", "development") == "development"
 
-# Specifies the `port` that Puma will listen on to receive requests; default is 3000.
-#
-port ENV.fetch("PORT") { 3000 }
+if ENV.fetch("RAILS_ENV", "development") == "development"
+  worker_timeout 3600
 
-# Specifies the `environment` that Puma will run in.
-#
-environment ENV.fetch("RAILS_ENV") { "development" }
+  # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
+  port ENV.fetch("PORT") { 3000 }
+
+  # Specifies the `environment` that Puma will run in.
+  environment ENV.fetch("RAILS_ENV") { "development" }
+end
+
+
+if ENV.fetch("RAILS_ENV", "production") == "production"
+  daemonize true
+  stdout_redirect "/var/log/puma/puma.stdout.log", "/var/log/puma/puma.stderr.log", true
+  bind "unix://#{Rails.root}/tmp/sockets/puma.sock"
+  environment ENV.fetch("RAILS_ENV") { "production" }
+end
 
 # Specifies the `pidfile` that Puma will use.
 pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
@@ -37,7 +46,7 @@ pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
 # before forking the application. This takes advantage of Copy On Write
 # process behavior so workers use less memory.
 #
-# preload_app!
+preload_app!
 
 # Allow puma to be restarted by `rails restart` command.
 plugin :tmp_restart
