fd1c74e49fff5d38667e3e9b840b2a51850ba402
gyvm <jolcde75+github@gmail.com>
2021-11-29 05:18:12 +0900

    chore: rails db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,54 +10,69 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_210_704_091_055) do
-  create_table 'events', charset: 'utf8mb4', collation: 'utf8mb4_0900_ai_ci', force: :cascade do |t|
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
-    t.string 'name'
-    t.text 'description'
-    t.string 'event_date'
+ActiveRecord::Schema.define(version: 2021_11_28_195151) do
+
+  create_table "active_admin_comments", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "namespace"
+    t.text "body"
+    t.string "resource_type"
+    t.bigint "resource_id"
+    t.string "author_type"
+    t.bigint "author_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["author_type", "author_id"], name: "index_active_admin_comments_on_author"
+    t.index ["namespace"], name: "index_active_admin_comments_on_namespace"
+    t.index ["resource_type", "resource_id"], name: "index_active_admin_comments_on_resource"
   end
 
-  create_table 'guest_possible_dates', charset: 'utf8mb4', collation: 'utf8mb4_0900_ai_ci', force: :cascade do |t|
-    t.bigint 'guest_id'
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
-    t.bigint 'possible_date_id'
-    t.integer 'status', null: false
-    t.index ['guest_id'], name: 'index_guest_possible_dates_on_guest_id'
-    t.index ['possible_date_id'], name: 'index_guest_possible_dates_on_possible_date_id'
+  create_table "events", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "name"
+    t.text "description"
+    t.string "event_date"
   end
 
-  create_table 'guests', charset: 'utf8mb4', collation: 'utf8mb4_0900_ai_ci', force: :cascade do |t|
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
-    t.string 'nickname'
-    t.text 'comment'
-    t.bigint 'event_id'
-    t.index ['event_id'], name: 'index_guests_on_event_id'
+  create_table "guest_possible_dates", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.bigint "guest_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.bigint "possible_date_id"
+    t.integer "status", null: false
+    t.index ["guest_id"], name: "index_guest_possible_dates_on_guest_id"
+    t.index ["possible_date_id"], name: "index_guest_possible_dates_on_possible_date_id"
   end
 
-  create_table 'possible_dates', charset: 'utf8mb4', collation: 'utf8mb4_0900_ai_ci', force: :cascade do |t|
-    t.string 'date', null: false
-    t.boolean 'deleted', default: false, null: false
-    t.bigint 'event_id'
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
-    t.index ['event_id'], name: 'index_possible_dates_on_event_id'
+  create_table "guests", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "nickname"
+    t.text "comment"
+    t.bigint "event_id"
+    t.index ["event_id"], name: "index_guests_on_event_id"
   end
 
-  create_table 'urls', charset: 'utf8mb4', collation: 'utf8mb4_0900_ai_ci', force: :cascade do |t|
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
-    t.string 'url'
-    t.bigint 'event_id'
-    t.index ['event_id'], name: 'index_urls_on_event_id'
+  create_table "possible_dates", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "date", null: false
+    t.boolean "deleted", default: false, null: false
+    t.bigint "event_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["event_id"], name: "index_possible_dates_on_event_id"
   end
 
-  add_foreign_key 'guest_possible_dates', 'guests'
-  add_foreign_key 'guest_possible_dates', 'possible_dates'
-  add_foreign_key 'guests', 'events'
-  add_foreign_key 'possible_dates', 'events'
-  add_foreign_key 'urls', 'events'
+  create_table "urls", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "url"
+    t.bigint "event_id"
+    t.index ["event_id"], name: "index_urls_on_event_id"
+  end
+
+  add_foreign_key "guest_possible_dates", "guests"
+  add_foreign_key "guest_possible_dates", "possible_dates"
+  add_foreign_key "guests", "events"
+  add_foreign_key "possible_dates", "events"
+  add_foreign_key "urls", "events"
 end
