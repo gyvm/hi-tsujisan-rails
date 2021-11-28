ae7b73841a96b8fc3542e411bfff5988ec619828
gyvm <jolcde75+github@gmail.com>
2021-11-29 07:20:43 +0900

    activate basic auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::Base
+  def authenticate_admin
+    authenticate_or_request_with_http_basic('Admin') do |name, password|
+      name == Settings.basic_auth.key && password == Settings.basic_auth.password
+    end
+  end
 end
